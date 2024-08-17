484ce61debe6d016b823e20c5d2b3d32c9f5572f
Aruneshkumar07 <aruneshoffl@gmail.com>
2024-08-17 11:15:31 +0530

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div id='first'>
+      <h1>Hii I am Aruneshkumar</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/footer.js
@@ -0,0 +1,10 @@
+import './App.css';
+function Footer() {
+  return (
+    <div id='Second'>
+      <h1>Hii I AM Aruneshkumar Full Stack Developer &copy; 2025 </h1>
+    </div>
+  );
+}
+
+export default Footer;
